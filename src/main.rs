5807060217503e7e3c56966b95ce5ce109f19cbe
pivovarvalera <pivovarvaleri@gmail.com>
2024-08-27 20:52:24 +0300

    Solution

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
-use std::f64::consts::PI;
-struct Angle{
-    fs: f64,
-    delta_alpha: f64,
-    alpha: f64
+struct Buffer{
+    array: Vec<i16>,
+    length: usize
 }
-impl Angle{
-    fn new() -> Angle{
-        Angle{
-            fs: 300000.,
-            delta_alpha: 0.,
-            alpha: 0.
+impl Buffer{
+    fn new(length: usize) -> Buffer{
+        Buffer{
+            array: Vec::with_capacity(length),
+            length
         }
     }
 
-    fn add(&mut self) -> i16{
-        self.delta_alpha = 2. * PI/self.fs;
-        println!("delta: {}", self.delta_alpha);
-        self.alpha += self.delta_alpha;
-        println!("alpha {}", self.alpha);
-        if(self.alpha > 2. * PI){
-            self.alpha -= 2. * PI;
+    fn add(&mut self, angle: f64) -> Option<Vec<i16>>{
+        match self.array.len() < self.length{
+            true => {
+                self.array.push(angle.round() as i16);
+                None
+            }
+            false => {
+                Some(self.array.clone())
+            }
         }
-        self.alpha.round() as i16
+
     }
 }
+fn main() {
 
-fn main() {}
+}
