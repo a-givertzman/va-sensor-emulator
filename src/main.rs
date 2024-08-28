35e2ed863e5837b01824cafc6b2f3629955f669a
pivovarvalera <pivovarvaleri@gmail.com>
2024-08-28 12:15:52 +0300

    Buffer | initial

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,22 @@
-struct Buffer{
-    array: Vec<i16>,
-    length: usize
-}
-impl Buffer{
-    fn new(length: usize) -> Buffer{
-        Buffer{
-            array: Vec::with_capacity(length),
-            length
-        }
-    }
-
-    fn add(&mut self, angle: f64) -> Option<Vec<i16>>{
-        match self.array.len() < self.length{
-            true => {
-                self.array.push(angle.round() as i16);
-                None
-            }
-            false => {
-                Some(self.array.clone())
-            }
-        }
-
-    }
-}
+mod buffer;
+mod angle;
+use angle::Angle;
+use buffer::Buffer;
+//
+//
 fn main() {
-
+    let buffer_len = 1024;      // to be loaded fgom the config.yaml
+    let sample_freq = 320_000;  // to be loaded fgom the config.yaml
+    let phase = 0.0;  // to be loaded fgom the config.yaml
+    let mut buffer = Buffer::new(buffer_len);
+    let mut angle = Angle::new(sample_freq, phase);
+    loop {
+        let angle_value = angle.add();
+        match buffer.add(angle_value) {
+            Some(pack) => {
+                // send buffer
+            }
+            None => {}
+        };
+    }
 }
