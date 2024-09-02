c79510eddd3c74450d667b6b0bcdb46476d6881f
pivovarvalera <pivovarvaleri@gmail.com>
2024-09-02 17:12:21 +0300

    VA-Sensor Emulator | Amplitude | test

--- src/amplitude.rs
+++ src/amplitude.rs
@@ -1,3 +1,5 @@
+use crate::angle::Angle;
+
 ///
 /// 
 pub struct Amplitude{
@@ -8,21 +10,28 @@ pub struct Amplitude{
 //
 impl Amplitude{
     ///
-    /// Creates new vector of amplitude
-    /// 
-    fn new() -> Self{
+    /// Creates `Amplitude` with
+    /// - `value` - value of the amplitude
+    /// - `len` - length of the buffer
+    pub fn new(len:usize) -> Self{
         Self{
-            value,
-            array,
+            value: 0.,
+            array: Vec::new(),
         }
     }
-    fn calc(&mut self, arrayA: Vec<i16>, arrayK: Vec<i16>, angle: f64) -> f64{
-        self.value = 0.0;
-        for i in 0..self.array.len() {
-            if (i< arrayK.len() && i < arrayA.len()){
-                self.value += arrayA[i] * (arrayK[i]*angle).sin();
+    ///
+    /// - Calculates new amplitude
+    /// - Returns calculated value
+    pub fn calc(&mut self, arrayA: &Vec<f64>, arrayK: &Vec<f64>, angle: f64) -> f64{
+        self.value = 0.;
+        for i in 0..arrayA.len(){
+        //for i in 0..self.array.len() {
+            if(i< arrayK.len() && i < arrayA.len()){
+                self.value += arrayA[i] * (arrayK[i]+angle).sin();
             }
         }
+        self.array.push(self.value as i16);
+        //println!("{:?}", self.array);
         self.value
     }
 }
