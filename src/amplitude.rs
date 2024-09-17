307213bb0eb95b5bc2adecb4a01aeb7e53168ee1
a-givertzman <a.givertzman@icloud.com>
2024-09-17 21:13:02 +0300

    Amplitude | prepared to yaml config

--- src/amplitude.rs
+++ src/amplitude.rs
@@ -1,32 +1,43 @@
-pub struct Amplitude{
-    value: f64,
-    array_a: Vec<f64>,
-    array_phi: Vec<f64>,
+///
+/// Amplitude ....
+/// 
+/// Conf Example:
+/// ```yaml
+///     # A:   ϕ, rad
+///      10.1: 0.0
+///      07.7: 0.0
+///     120.0: 0.0
+/// ```
+pub struct Amplitude {
+    /// `Vec<(A, ϕ)>`
+    params: Vec<(f64, f64)>,
 }
 //
 //
-impl Amplitude{
+impl Amplitude {
     ///
     /// Creates `Amplitude` with
-    /// - `value` - value of the amplitude
-    /// - `len` - length of the buffer
-    pub fn new(array_a: Vec<f64>, array_phi: Vec<f64>) -> Self{
-        Self{
-            value: 0.,
-            array_a,
-            array_phi,
+    /// - `conf` - yaml value containing a configuration
+    ///
+    /// ```yaml
+    ///     # A:   ϕ, rad
+    ///      10.1: 0.0
+    ///      07.7: 0.0
+    ///     120.0: 0.0
+    /// ```
+    pub fn new(params: Vec<(f64, f64)>) -> Self {
+        Self {
+            params,
         }
     }
     ///
     /// - Calculates new amplitude
     /// - Returns calculated value
-    pub fn calc(&mut self, angle: f64) -> f64{
-        self.value = 0.;
+    pub fn calc(&self, angle: f64) -> f64 {
         log::debug!("angle: {}", angle);
-        for i in 0..self.array_a.len(){
-            self.value += self.array_a[i]*(angle + self.array_phi[i]).sin();
-        }
-        self.value
+        self.params.iter().fold(0.0, |value, (amp, phi)| {
+            value + *amp * (angle + *phi).sin()
+        })
     }
 
 }
--- src/tests/amplitude_test.rs
+++ src/tests/amplitude_test.rs
@@ -1,11 +1,12 @@
 #[cfg(test)]
 //сделать класс и формулы для комплексного числа
 mod amplitude {
-    use std::{array, borrow::Borrow, f64::consts::PI, result, sync::Once, time::{Duration, Instant}};
+    use std::{f64::consts::PI, sync::Once, time::Duration};
+    use rand::Rng;
     use testing::stuff::max_test_duration::TestDuration;
     use debugging::session::debug_session::{DebugSession, LogLevel, Backtrace};
 
-    use crate::{amplitude::{self, Amplitude}, angle::Angle, stuff::approx_eq::AproxEq, complex::Complex};
+    use crate::{amplitude::Amplitude, stuff::approx_eq::AproxEq};
     ///
     ///
     static INIT: Once = Once::new();
@@ -37,52 +38,50 @@ mod amplitude {
         log::debug!("\n{}", self_id);
         let test_duration = TestDuration::new(self_id, Duration::from_secs(10));
         test_duration.run().unwrap();
-
-        //do not change capacity during test
-        let array_a = vec![1., 2., 3.];
-        let array_phi = vec![0.1, 0.2, 0.3];
-
-        log::debug!("array_a: {:?} \narray_phi: {:?}", array_a, array_phi);
-            
-        //manually calculated values
+        // Manually calculated values
         let test_data = [
-            (90.*PI/180., 5.8211), //(angle in radians, calculated amplitude)
-            (234.*PI/180., 0.),
-            (180.*PI/180., -1.3837),
-            (270.*PI/180., -5.8211),
-            (360.*PI/180., 1.3837),
-            (0.*PI/180., 1.3837),
-            (45.*PI/180., 5.0946),
-            (30.*PI/180., 4.1089),
+            ( 90.*PI/180., 5.821146788337327), //(angle in radians, calculated amplitude)
+            (234.*PI/180., -5.5227443516451995),
+            (180.*PI/180., -1.3837326982209683),
+            (270.*PI/180., -5.821146788337327),
+            (360.*PI/180., 1.3837326982209674),
+            (  0.*PI/180., 1.3837326982209692),
+            ( 45.*PI/180., 5.094619142577222),
+            ( 30.*PI/180., 4.1089210628752095),
         ];
-
-        //the values calculated by static formula
-        // let test_data = [
-        //     (90.*PI/180., formula(90.*PI/180., &array_a, &array_phi)), //(angle in radians, calculated amplitude)
-        //     (180.*PI/180., formula(180.*PI/180., &array_a, &array_phi)),
-        //     (270.*PI/180., formula(270.*PI/180., &array_a, &array_phi)),
-        //     (360.*PI/180., formula(360.*PI/180., &array_a, &array_phi)),
-        //     (0.*PI/180., formula(0.*PI/180., &array_a, &array_phi)),
-        //     (45.*PI/180., formula(45.*PI/180., &array_a, &array_phi)),
-        //     (30.*PI/180., formula(30.*PI/180., &array_a, &array_phi)),
-        // ];
-        
+        let conf: serde_yaml::Value = serde_yaml::from_str(r#"
+            1.0: 0.1
+            2.0: 0.2
+            3.0: 0.3
+        "#).unwrap();
+        let amplitude = Amplitude::new(build_apmlitude_params(&conf));
         for (angle, target) in test_data {
-            log::debug!("amplitude: {}", target);
-            let mut amplitude = Amplitude::new(array_a.clone(), array_phi.clone()); //(array_a/k.len());
-            let mut result = amplitude.calc(angle);
-            log::debug!("angle {} \nresult: {:?}\ntarget: {:?}", angle *180./PI, result, target);
-            assert!(result.aprox_eq(target, 3));
+            let result = amplitude.calc(angle);
+            log::debug!("angle {:>7.3},  result: {:.5},  target: {:.5}", angle *180./PI, result, target);
+            assert!(result.aprox_eq(target, 3), "angle {} \nresult: {:?}\ntarget: {:?}", angle *180./PI, result, target);
         }
         test_duration.exit();
     }
-    
-    fn cycle_formula(alpha: f64, array_a:&Vec<f64>, array_phi: &Vec<f64>) -> f64{
-        let mut value = 0.;
-        for i in 0..array_a.len(){
-            value += array_a[i]*(alpha + array_phi[i]).sin();
+    ///
+    /// 
+    fn build_apmlitude_params(conf: &serde_yaml::Value) -> Vec<(f64, f64)> {
+        match conf.as_mapping() {
+            Some(conf) => {
+                conf.into_iter().map(|(key, value)| {
+                    let amp = key.as_f64().unwrap_or_else(|| panic!("build_apmlitude_params | Invalid f64 amplitude value '{:?}' in config {:#?}", key, conf));
+                    let phi = value.as_f64().unwrap_or_else(|| panic!("build_apmlitude_params | Invalid f64 phi value '{:?}' in config {:#?}", value, conf));
+                    (amp, phi)
+                }).collect()
+            }
+            None => panic!("build_apmlitude_params | Invalid config {:#?}", conf),
         }
-        value
+    }
+    ///
+    /// 
+    fn cycle_formula(alpha: f64, params: &Vec<(f64, f64)>) -> f64{
+        params.iter().fold(0.0, |value, (amp, phi)| {
+            value + *amp * (alpha + *phi).sin()
+        })
     }
     ///
     /// Testing amplitude on `random` values
@@ -95,50 +94,37 @@ mod amplitude {
         log::debug!("\n{}", self_id);
         let test_duration = TestDuration::new(self_id, Duration::from_secs(10));
         test_duration.run().unwrap();
-        use rand::{Rng, thread_rng};
-
-        let array_capacity = thread_rng().gen_range(1..=100);
-        let mut array_a = vec![0.; array_capacity];
-        let mut array_phi = vec![0.; array_capacity];
-
-        for i in 0..array_capacity{
-            array_a.push(thread_rng().gen_range(0.0..=1000.0));
-            array_phi.push(thread_rng().gen_range(0.0..=1000.0));
-        }
-
-        let data_capacity = thread_rng().gen_range(1..=100);
-        let mut test_data :Vec<(f64, f64)>= Vec::new();
-        let mut rand_angle = 0.;
-        let mut calc_amplitude = 0.;
-
-        let mut num_type = false; //true - Real, false - Complex
-
-        for i in 0..data_capacity{
-            num_type = thread_rng().gen();
-            log::debug!("num_type: {}", num_type);
-
-            if(num_type){
-                rand_angle = thread_rng().gen_range(0.0..=360.0) * PI/180.;
-                log::debug!("real angle in rad: {}", rand_angle);
-            }
-            else{
-                let re = thread_rng().gen_range(0.0..=100.0);
-                let im = thread_rng().gen_range(0.0..=100.0);
-                let mut complex = Complex::new(re, im);
-                log::debug!("complex number: {} + {}i", re, im);
-                rand_angle = complex.arg();
-                log::debug!("complex angle in rad: {}", rand_angle);
-                log::debug!("complex angle in grad: {}", rand_angle * 180./PI);
-            }
-            calc_amplitude  = cycle_formula(rand_angle, &array_a, &array_phi);
-
-            test_data.push((rand_angle, calc_amplitude));
-        }
-
+        ////////////////////////////////////////////////// 
+        // ============== Configurations ============== //
+        // Sampling freq - defines steps of calculations: Δα = 2π / freq
+        let sampling_freq = 100;
+        let params_count = 3;
+        // ============== Configurations ============== //
+        ////////////////////////////////////////////////// 
+        let mut rng = rand::thread_rng();
+        let params = (0..params_count).fold(vec![], |mut params, i| {
+            let amp = rng.gen_range(0.01..10.0);
+            let phi = rng.gen_range(0.00..2.0 * PI);
+            params.push((amp, phi));
+            params
+        });
+        let conf = params.iter().fold(String::new(), |mut conf, (amp, phi)| {
+            conf.push_str(&format!(r#"{}: {}
+"#, amp, phi));
+            conf
+        });
+        log::debug!("conf: {:#?}", conf);
+        let conf: serde_yaml::Value = serde_yaml::from_str(&conf).unwrap();
+        log::debug!("conf: {:#?}", conf);
+        let test_data = (0..sampling_freq * 3).map(|step| {
+            let alpha = (step as f64) * 2.0 * PI / (sampling_freq as f64);
+            let value = cycle_formula(alpha, &params);
+            (alpha, value)
+        });
+        let amplitude = Amplitude::new(build_apmlitude_params(&conf));
         for (angle, target) in test_data {
             log::debug!("amplitude: {}", target);
-            let mut amplitude = Amplitude::new(array_a.clone(), array_phi.clone());
-            let mut result = amplitude.calc(angle);
+            let result = amplitude.calc(angle);
             log::debug!("angle {} \nresult: {:?}\ntarget: {:?}", angle *180./PI, result, target);
             assert!(result.aprox_eq(target, 3));
         }
