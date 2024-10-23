40732ceb905bdedb5f00a8479616ebcfa5281b11
pivovarvalera <pivovarvaleri@gmail.com>
2024-10-23 23:06:18 +0300

    VA-Sensor | Emulator | UdpMessage

---
+++ src/main_service/udp_message.rs
@@ -0,0 +1,36 @@
+use crate::UpdHeader; 
+///
+/// Struct UpdMessage
+/// - header - contains the UPD header information
+/// - data - array of values
+pub struct UpdMessage{
+    pub header: UdpHeader,
+    pub data: Vec<u8>,
+}
+//
+//
+impl UpdMessage{
+    ///
+    /// Creates a message for udp
+    pub fn new(header: UdpHeader, data: Vec<u8>) -> Self{
+        Self{
+            header,
+            data,
+        }
+    }
+    ///
+    /// Convert fields of UdpMessage to Vector
+    pub fn message(&self) -> Vec<u8>{
+        let mut message_bytes = Vec::new();
+
+        for bytes in self.header.to_bytes(){
+            message_bytes.push(bytes);
+        }
+
+        for &bytes in &self.data{
+            message_bytes.push(bytes);
+        }
+
+        message_bytes
+    }
+}
