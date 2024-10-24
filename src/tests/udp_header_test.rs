81bcf3aed36f55c5dc59becd604b0031338df9cf
pivovarvalera <pivovarvaleri@gmail.com>
2024-10-24 18:44:39 +0300

    VA-Sensor | Emulator | UdpHeader | test

--- src/tests/udp_header_test.rs
+++ src/tests/udp_header_test.rs
@@ -1,10 +1,9 @@
 #[cfg(test)]
 
-mod tests {
+mod udp_header {
     use std::{sync::Once, time::{Duration, Instant}};
     use testing::stuff::max_test_duration::TestDuration;
     use debugging::session::debug_session::{DebugSession, LogLevel, Backtrace};
-
     use crate::main_service::udp_header::UdpHeader;
     ///
     ///
@@ -23,7 +22,7 @@ mod tests {
     ///
     /// Testing such functionality / behavior
     #[test]
-    fn test_udp_header() {
+    fn test_to_bytes() {
         DebugSession::init(LogLevel::Info, Backtrace::Short);
         init_once();
         init_each();
@@ -32,26 +31,19 @@ mod tests {
         log::debug!("\n{}", self_id);
         let test_duration = TestDuration::new(self_id, Duration::from_secs(1));
         test_duration.run().unwrap();
-        let test_data:[Vec<(u8, u8, u8, u8)>; 4] = [
-            (vec![(0, 0, 16, 255)]),
-            (vec![(1, 22, 32, 100)]),
-            (vec![(0, 0, 16, 0)]),
-            (vec![(1, 22, 64, 100)]),
+        let test_data: [((u8, u8, u8, u8), Vec<u8>); 2] = [
+            ((0, 0, 16, 255), vec![0, 0, 16, 255]),
+            ((0, 0, 0, 0), vec![0, 0, 0, 0]),
         ];
 
-        for (step, conf) in test_data.iter().enumerate(){
-            let (syn, addr, r#type, count) = conf[0];
-            let header = UdpHeader::new(syn, addr, r#type, count);
+        for (step, (data, target)) in test_data.iter().enumerate(){
+            let header = UdpHeader::new(data.0, data.1, data.2, data.3);
             println!("step: {}", step);
             println!("syn: {}, addr: {:?}, type: {:?}, count: {:?}\n", header.syn, header.addr, header.r#type, header.count);
             let result = header.to_bytes();
-            let mut target = Vec::new();
-            target.push(syn);
-            target.push(addr);
-            target.push(r#type);
-            target.push(count);
+            println!("result: {:?}, target: {:?}", result, target);
             log::debug!("result: {:?}, target: {:?}", result, target);
-            assert!(result == target, "step {} \nresult: {:?}\ntarget: {:?}", step, result, target);
+            assert!(result == *target, "step {} \nresult: {:?}\ntarget: {:?}", step, result, target);
         }
         test_duration.exit();
     }
