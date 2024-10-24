7295f2f3bfcfe6ba266d30b77200da0ee94dd09e
pivovarvalera <pivovarvaleri@gmail.com>
2024-10-24 18:17:09 +0300

    VA-Sensor | Emulator | UdpMessage | test

--- src/tests/udp_message_test.rs
+++ src/tests/udp_message_test.rs
@@ -1,10 +1,9 @@
 #[cfg(test)]
 
-mod tests {
+mod udp_message {
     use std::{sync::Once, time::{Duration, Instant}};
     use testing::stuff::max_test_duration::TestDuration;
     use debugging::session::debug_session::{DebugSession, LogLevel, Backtrace};
-
     use crate::main_service::udp_header::UdpHeader;
     use crate::main_service::udp_message::UpdMessage;
     ///
@@ -24,7 +23,7 @@ mod tests {
     ///
     /// Testing such functionality / behavior
     #[test]
-    fn test_udp_header() {
+    fn test_to_bytes() {
         DebugSession::init(LogLevel::Info, Backtrace::Short);
         init_once();
         init_each();
@@ -34,18 +33,14 @@ mod tests {
         let test_duration = TestDuration::new(self_id, Duration::from_secs(1));
         test_duration.run().unwrap();
         let test_data = [
-            (UdpHeader::new(0, 0, 64, 255), vec![]),
-            (UdpHeader::new(22, 128, 16, 128), vec![89, 60, 0]),
-            (UdpHeader::new(12, 67, 32, 78), vec![0, 9]),
-            (UdpHeader::new(7, 9, 128, 0), vec![0, 0, 0]),
+            (UdpHeader::new(0, 0, 64, 255), vec![], vec![0, 0, 64, 255]),
+            (UdpHeader::new(22, 128, 16, 128), vec![89, 60, 0], vec![22, 128, 16, 128, 89, 60, 0]),
+            (UdpHeader::new(12, 67, 32, 78), vec![0, 9], vec![12, 67, 32, 78, 0, 9]),
+            (UdpHeader::new(7, 9, 128, 0), vec![0, 0, 0], vec![7, 9, 128, 0, 0, 0, 0]),
         ];
-
-        for(step, (header, data)) in test_data.into_iter().enumerate(){
+        for(step, (header, data, target)) in test_data.into_iter().enumerate(){
             let message = UpdMessage::new(UdpHeader::new(header.syn, header.addr, header.r#type, header.count), data.clone());
             let result = message.message();
-            let mut target = header.to_bytes();
-            target.extend_from_slice(&data);
-            println!("step {} \nresult: {:?}\ntarget: {:?}", step, result, target);
             log::debug!("step {} \nresult: {:?}\ntarget: {:?}", step, result, target);
             assert!(result == target, "step {} \nresult: {:?}\ntarget: {:?}", step, result, target);
         }
