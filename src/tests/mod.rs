b147fb655041a1d19a5c2fe190834f2578afd880
pivovarvalera <pivovarvaleri@gmail.com>
2024-10-27 17:57:49 +0300

    Back | VA-Sensor | Emulator | UdpMessage | Correct | test

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -3,3 +3,4 @@ mod angle_test;
 mod amplitude_test;
 mod main_service_config_test;
 mod udp_header_test;
+mod upd_message_test;
---
+++ src/tests/upd_message_test.rs
@@ -0,0 +1,50 @@
+#[cfg(test)]
+
+mod udp_message {
+    use std::{sync::Once, time::{Duration, Instant}};
+    use testing::stuff::max_test_duration::TestDuration;
+    use debugging::session::debug_session::{DebugSession, LogLevel, Backtrace};
+    use crate::main_service::upd_header::UdpHeader;
+    use crate::main_service::upd_message::UpdMessage;
+    ///
+    ///
+    static INIT: Once = Once::new();
+    ///
+    /// once called initialisation
+    fn init_once() {
+        INIT.call_once(|| {
+            // implement your initialisation code to be called only once for current test file
+        })
+    }
+    ///
+    /// returns:
+    ///  - ...
+    fn init_each() -> () {}
+    ///
+    /// Testing such functionality / behavior
+    #[test]
+    fn test_build_message() {
+        DebugSession::init(LogLevel::Info, Backtrace::Short);
+        init_once();
+        init_each();
+        log::debug!("");
+        let self_id = "test";
+        log::debug!("\n{}", self_id);
+        let test_duration = TestDuration::new(self_id, Duration::from_secs(1));
+        test_duration.run().unwrap();
+        let test_data = [
+            (UdpHeader::new(0, 0, 64, 255), vec![], vec![0, 0, 64, 255]),
+            (UdpHeader::new(22, 128, 16, 128), vec![89, 60, 0], vec![22, 128, 16, 128, 89, 60, 0]),
+            (UdpHeader::new(12, 67, 32, 78), vec![0, 9], vec![12, 67, 32, 78, 0, 9]),
+            (UdpHeader::new(7, 9, 128, 0), vec![0, 0, 0], vec![7, 9, 128, 0, 0, 0, 0]),
+        ];
+
+        for(step, (header, data, target)) in test_data.into_iter().enumerate(){
+            let message = UpdMessage::new(UdpHeader::new(header.syn, header.addr, header.r#type, header.count), data.clone());
+            let result = message.build_message();
+            log::debug!("step {} \nresult: {:?}\ntarget: {:?}", step, result, target);
+            assert!(result == target, "step {} \nresult: {:?}\ntarget: {:?}", step, result, target);
+        }
+        test_duration.exit();
+    }
+}
